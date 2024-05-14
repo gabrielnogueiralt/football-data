90c1681b5437ad64fd816953c3c9f8d3275780ad
gabrielnogueiralt <gabrielnogueiraleite@gmail.com>
2024-05-14 00:26:41 -0300

    refactor: filter top 5 leagues based on predefined league codes

--- src/stores/football-data.ts
+++ src/stores/football-data.ts
@@ -1,22 +1,26 @@
 import { defineStore } from 'pinia';
 import ApiService from '../services/api-service';
+import type { Competition, LeagueDetails, TeamDetails } from '../types/football-data';
 
 export const useFootballDataStore = defineStore('footballData', {
   state: () => ({
-    top5Leagues: [],
-    leagueDetails: {},
-    teamDetails: {},
+    top5Leagues: [] as Competition[],
+    leagueDetails: {} as LeagueDetails | null,
+    teamDetails: {} as TeamDetails | null,
   }),
   getters: {
-    top5Leagues: (state) => state.top5Leagues,
-    leagueDetails: (state) => state.leagueDetails,
-    teamDetails: (state) => state.teamDetails,
+    getTop5Leagues: (state) => state.top5Leagues,
+    getLeagueDetails: (state) => state.leagueDetails,
+    getTeamDetails: (state) => state.teamDetails,
   },
   actions: {
     async fetchTop5Leagues() {
       try {
         const response = await ApiService.getTierOneLeagues();
-        this.top5Leagues = response.data.competitions;
+        const top5LeagueCodes = ['PL', 'BL1', 'PD', 'SA', 'FL1'];
+        this.top5Leagues = response.data.competitions.filter((competition: Competition) =>
+          competition.code && top5LeagueCodes.includes(competition.code)
+        );
       } catch (error) {
         console.error('Failed to fetch top 5 leagues:', error);
       }

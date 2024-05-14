569423a403a01bcb6a531c185d6b67e97302c908
gabrielnogueiralt <gabrielnogueiraleite@gmail.com>
2024-05-14 00:26:18 -0300

    refactor: update API service to use consistent endpoint paths

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -1,27 +1,29 @@
 import axios from 'axios';
 
 const BASE_URL = '/api';
-
 const API_TOKEN = import.meta.env.VITE_API_TOKEN;
 
 const instance = axios.create({
   baseURL: BASE_URL,
   headers: {
-    'X-Auth-Token': API_TOKEN
-  }
+    'X-Auth-Token': API_TOKEN,
+  },
 });
 
 export default {
   getTierOneLeagues() {
-    return instance.get('competitions?plan=TIER_ONE&areas=2077');
+    return instance.get('/competitions?plan=TIER_ONE&areas=2077');
   },
   getLeagueDetails(id: number) {
-    return instance.get(`competitions/${id}/standings`);
+    return instance.get(`/competitions/${id}/standings`);
+  },
+  getLeagueTeams(id: number) {
+    return instance.get(`/competitions/${id}/teams`);
+  },
+  getLeagueStandings(id: number) {
+    return instance.get(`/competitions/${id}/standings`);
   },
   getTeam(id: number) {
-    return instance.get(`teams/${id}`);
-  },
-  getTeamPlayers(id: number) {
-    return instance.get(`teams/${id}/players`);
+    return instance.get(`/teams/${id}`);
   }
 };

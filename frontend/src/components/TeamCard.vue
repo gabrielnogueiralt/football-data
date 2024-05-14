4dafb089dc8a611573e7c9f412b8bc07cf246c44
gabrielnogueiralt <gabrielnogueiraleite@gmail.com>
2024-05-14 02:43:29 -0300

    refact: update TeamCard component to include favorite button functionality

--- frontend/src/components/TeamCard.vue
+++ frontend/src/components/TeamCard.vue
@@ -1,25 +1,44 @@
+<!-- components/TeamCard.vue -->
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { defineProps, computed } from 'vue';
 import { useRouter } from 'vue-router';
+import { useFootballDataStore } from '../stores/football-data';
 import { Team } from '../types/football-data';
 
 const props = defineProps<{ team: Team }>();
+const footballDataStore = useFootballDataStore();
 const router = useRouter();
 
 const navigateToTeam = (id: number) => {
   router.push({ name: 'team-players', params: { id } });
 };
+
+const isFavorite = computed(() => footballDataStore.favorites.includes(props.team.id));
+
+const toggleFavorite = async (teamId: number) => {
+  if (isFavorite.value) {
+    await footballDataStore.removeFavorite(teamId);
+  } else {
+    await footballDataStore.saveFavorite(teamId);
+  }
+};
 </script>
 
 <template>
-  <div class="card team-card m-2" @click="navigateToTeam(props.team.id)">
+  <div class="card team-card m-2 shadow-sm border-0" @click="navigateToTeam(props.team.id)">
     <img :src="props.team.crest" class="card-img-top" :alt="props.team.name">
-    <div class="card-body">
-      <h5 class="card-title text-center mb-2">{{ props.team.name }}</h5>
-      <p class="card-text text-center text-muted">
+    <div class="card-body text-center">
+      <h5 class="card-title mb-2">{{ props.team.name }}</h5>
+      <p class="card-text text-muted">
         <strong>Venue:</strong> {{ props.team.venue }}<br>
         <strong>Founded:</strong> {{ props.team.founded }}
       </p>
+      <div class="d-flex justify-content-center">
+        <button @click.stop="toggleFavorite(props.team.id)" class="btn btn-sm"
+          :class="isFavorite ? 'btn-danger' : 'btn-outline-primary'">
+          {{ isFavorite ? 'Remove from Favorites' : 'Add to Favorites' }}
+        </button>
+      </div>
     </div>
   </div>
 </template>

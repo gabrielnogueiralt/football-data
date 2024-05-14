c40e188d2354fa99c54942b2b248052067eada7d
gabrielnogueiralt <gabrielnogueiraleite@gmail.com>
2024-05-14 00:27:32 -0300

    feat: create LoadingSpinner component

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="d-flex justify-content-center mt-5">
+    <div class="spinner-border" role="status"></div>
+  </div>
+</template>
+
+<style scoped>
+.spinner-border {
+  width: 3rem;
+  height: 3rem;
+}
+</style>
